2d0a4bcd6f2bf4ea67c9afae59ae59469f7ec1da
Muba97 <mubakhan1997@gmail.com>
2020-05-30 12:20:40 -0500

    adding the logo in the navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import { AppBar, Button, makeStyles } from '@material-ui/core';
+import { Toolbar, AppBar, IconButton, ButtonBase, makeStyles } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
-const useStyles = makeStyles()
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appbar: {
+    background: 'black',
+  },
+  button: {
+    color: 'inherit',
+    textDecorationLine: 'none',
+    '&:hover': {
+      color: 'white',
+    },
+  },
+  smallLogo: {
+    height: 50,
+  },
+}));
 
 const Navbar = () => {
+  const classes = useStyles();
   return (
     <div>
-        <AppBar position="Static">
-            
-        </AppBar>
+      <AppBar position="Static" className={classes.appbar}>
+        <Toolbar>
+          <Link to="/">
+            <ButtonBase>
+              <img
+                alt="logo-icon"
+                src="/Anayah-Logo-White.png"
+                className={classes.smallLogo}
+              />
+            </ButtonBase>
+          </Link>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
