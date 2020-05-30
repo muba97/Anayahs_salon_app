fe49d44946e024c96b6508df08cc315c238d9fa7
Muba97 <mubakhan1997@gmail.com>
2020-05-30 11:42:25 -0500

    added eslint, routing, init navbar components

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AppBar, Button, makeStyles } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles()
+
+const Navbar = () => {
+  return (
+    <div>
+        <AppBar position="Static">
+            
+        </AppBar>
+    </div>
+  );
+};
+
+export default Navbar;
