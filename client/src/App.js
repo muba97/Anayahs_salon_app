c6ee5d2cf3c11dd36dde9fec8e577d43369eeb5b
muba97 <42083167+muba97@users.noreply.github.com>
2020-05-30 12:50:04 -0500

    Profile page (#4)
    * added eslint, routing, init navbar components
    
    * adding the logo in the navbar
    
    * fixing package-lock.json issue and fixed app.test.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Profile from './Pages/Profile';
+import Navbar from './Components/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-testid="app">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Toolbar, AppBar, IconButton, ButtonBase, makeStyles } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appbar: {
+    background: 'black',
+  },
+  button: {
+    color: 'inherit',
+    textDecorationLine: 'none',
+    '&:hover': {
+      color: 'white',
+    },
+  },
+  smallLogo: {
+    height: 50,
+  },
+}));
+
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="Static" className={classes.appbar}>
+        <Toolbar>
+          <Link to="/">
+            <ButtonBase>
+              <img
+                alt="logo-icon"
+                src="/Anayah-Logo-White.png"
+                className={classes.smallLogo}
+              />
+            </ButtonBase>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Navbar;
