28e3795cc0d1c498cd5813ef9736ccfea38f408d
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2020-06-13 15:01:47 -0500

    Updated dotenv syntax to import style syntax

--- server/src/server.js
+++ server/src/server.js
@@ -1,34 +1,36 @@
 import { ApolloServer, gql } from 'apollo-server-express';
 import express from 'express';
 import mongoose from 'mongoose';
-require('dotenv').config();
+import dotenv from 'dotenv';
 
-  const typeDefs = gql`
-  type Query {
-    hello: String!
+dotenv.config();
+
+const typeDefs = gql`
+type Query {
+  hello: String!
+}
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'hi'
   }
-  `;
+};
 
-  const resolvers = {
-    Query: {
-      hello: () => 'hi'
-    }
-  };
-  const app = express();
+const app = express();
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers
-  });
-  
-  server.applyMiddleware({ app });
-  
-  mongoose.connect(process.env.MONGO_URI, { 
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
+server.applyMiddleware({ app });
 
-  app.listen({ port: 4000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-  );
+mongoose.connect(process.env.MONGO_URI, { 
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
