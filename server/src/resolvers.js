07d597d57e959fab5cb3083e57a8f377db0d2813
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2020-06-14 11:43:06 -0500

    created typeDefs and resolvers file, and updated init server code

---
+++ server/src/resolvers.js
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    hello: () => 'hi',
+  },
+};
+
+export default resolvers;
--- server/src/server.js
+++ server/src/server.js
@@ -2,39 +2,33 @@ import { ApolloServer, gql } from 'apollo-server-express';
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import typeDefs from './typeDefs';
+import resolvers from './resolvers';
 
 dotenv.config();
 
-const typeDefs = gql`
-  type Query {
-    hello: String!
-  }
-`;
+const startServer = async () => {
+  const app = express();
 
-const resolvers = {
-  Query: {
-    hello: () => 'hi',
-  },
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  server.applyMiddleware({ app });
+
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+    .then(() => console.log('Mongodb connected'))
+    .catch((err) => console.log(err));
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
 };
 
-const app = express();
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-server.applyMiddleware({ app });
-
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-  })
-  .then(() => console.log('Mongodb connected'))
-  .catch((err) => console.log(err));
-
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+startServer();
