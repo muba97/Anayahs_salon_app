46e6517c62e19f6849d06fbdc6c86c36cc9ba2ed
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2020-06-14 11:50:05 -0500

    updated sum test and deleted routes folder

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,4 @@
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
