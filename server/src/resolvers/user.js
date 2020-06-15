b2a6e5300eb33199ecfde33936bf19e39ed42fb2
hectorrami <hramirez4@uh.edu>
2020-06-15 11:26:03 -0500

    setting up the arguments for the user

--- server/src/resolvers/user.js
+++ server/src/resolvers/user.js
@@ -1,9 +1,23 @@
+import mongoose from 'mongoose';
+import { UserInputError } from 'apollo-server-express';
+import { User } from '../models';
+
 export default {
   Query: {
-    users: (root, arg, context, info) => {},
-    user: (root, arg, context, info) => {},
+    users: (root, arg, context, info) => {
+      return User.find({});
+    },
+
+    user: (root, { id }, arg, context, info) => {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new UserInputError(`${id} is not a valid user ID`);
+      }
+      return User.findById(id);
+    },
   },
   Mutation: {
-    signUp: (root, arg, context, info) => {},
+    signUp: (root, args, context, info) => {
+      return User.create(args);
+    },
   },
 };
