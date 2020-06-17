72fa423536f282269097ff043d9714f8d6f7690f
Muba97 <mubakhan1997@gmail.com>
2020-06-17 18:56:59 -0500

    Tested mutation signUp

--- server/tests/typeDefs/user.test.js
+++ server/tests/typeDefs/user.test.js
@@ -35,7 +35,45 @@ describe('User typeDefs Test', () => {
             email: 'muba@fsdf.com',
             phoneNumber: '832-222-2222',
           },
+          {
+            id: '5eeaab34560a1080adf18f4f',
+            firstName: 'Mubashir',
+            lastName: 'Khan',
+            email: 'muba@email.com',
+            phoneNumber: '832-222-2222',
+          },
         ],
+      },
+    });
+  });
+
+  test('mutations', async () => {
+    const response = await axios.post('http://localhost:4000/graphql', {
+      query: `
+      mutation {
+        signUp(firstName:"Mubashir", lastName:"Khan", email:"muba@gmail.com", birthDay: "10/12/2020", phoneNumber: "832-222-2222", password:"hello"){
+          id
+          firstName
+          lastName
+          email
+          birthDay
+          phoneNumber
+          password
+        }
+      }
+      `,
+    });
+    const { data } = response;
+    expect(data).toMatchObject({
+      data: {
+        signUp: {
+          firstName: 'Mubashir',
+          lastName: 'Khan',
+          email: 'muba@gmail.com',
+          birthDay: '10/12/2020',
+          phoneNumber: '832-222-2222',
+          password: 'hello',
+        },
       },
     });
   });
