e09214d486e2339e6a9b083856468c0ea6f3ec21
muba97 <42083167+muba97@users.noreply.github.com>
2020-07-10 12:08:18 -0500

    Frontend backend connection (#18)
    * [INIT] :pencil: inital setup for apollo client
    
    * [FIX] :wrench: fixed the error with link and cache, backend and frontend is connecting now
    
    Co-authored-by: Fahad Siddiqi <fahad.a.sidd@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,53 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient, gql } from 'apollo-boost';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from "apollo-cache-inmemory";
 import ProfilePage from './Pages/ProfilePage';
 import Navbar from './Components/Navbar';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:60001/graphql',
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+client
+  .query({
+    query: gql`
+      {
+        users {
+          id
+          firstName
+          lastName
+          email
+          phoneNumber
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
 
 const App = () => {
   return (
     <div className="App" data-testid="app">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <ProfilePage />
-          </Route>
-        </Switch>
-      </Router>
+      <ApolloProvider client={client}>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <ProfilePage />
+            </Route>
+          </Switch>
+        </Router>
+      </ApolloProvider>
     </div>
   );
 };
