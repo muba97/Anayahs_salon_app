783edc0941c60b28f2fa5cc0abe663981c177f7f
Muba97 <mubakhan1997@gmail.com>
2020-07-13 22:53:54 -0500

    [STYLE][REFACTOR] :<haircut>: styling the services once dropdown component is opened as well as restructuing the serviceInfo object and its div

--- client/src/Components/service.js
+++ client/src/Components/service.js
@@ -6,8 +6,40 @@ const useStyle = makeStyles({
   field: {
     margin: 10,
   },
-  p: {
-    textAlign: 'right',
+  item: {
+    width: '85%',
+    display: 'block',
+    fontSize: '15px',
+    boxSizing: 'border-box',
+    borderRadius: '1px',
+    border: '1px solid black',
+    padding: '8px 11px',
+    marginBottom: 3,
+    float: 'left',
+    '&:disabled': {
+      opacity: '40%',
+    },
+  },
+  smallBtn: {
+    justifyContent: 'center',
+    display: 'flex',
+    background: '#000000',
+    borderLeft: '1px solid',
+    borderRight: '1px solid',
+    color: 'white',
+    fontFamily: ['Montserrat', 'sans-serif'].join(','),
+    width: '15%',
+    padding: '20px 25px 20px 25px',
+    fontSize: '13px',
+    '&:disabled': {
+      opacity: '40%',
+    },
+    '&:hover': {
+      opacity: '70%',
+    },
+  },
+  span: {
+    textAlign: 'left',
     display: 'block',
     boxSizing: 'border-box',
     width: '100%',
@@ -21,6 +53,7 @@ const useStyle = makeStyles({
       opacity: '40%',
     },
   },
+
   btn: {
     justifyContent: 'space-between',
     textAlign: 'left',
@@ -40,14 +73,21 @@ const useStyle = makeStyles({
     },
   },
 });
-const Services = ({ serviceInfo }) => {
+const Services = ({ label, items }) => {
   const [open, setOpen] = useState(false);
+  const [serviceData, setServiceData] = useState(items);
   const [edit, setEdit] = useState([]);
   const toggle = () => setOpen(!open);
 
-  function handleOnClick(item) {}
-  console.log('Hello World')
+  const handleChange = (e) => {
+    setServiceData({
+      ...serviceData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
+  function handleOnClick(item) {}
+  console.log('Hello World');
 
   const classes = useStyle();
   return (
@@ -61,14 +101,33 @@ const Services = ({ serviceInfo }) => {
         className={classes.btn}
       >
         <div className={classes.field}>
-          <p className={classes.field}>{serviceInfo}</p>
+          <p className={classes.field}>{label}</p>
         </div>
-        <div className ={classes.field}>
+        <div className={classes.field}>
           <p>{open ? 'Close' : 'Open'}</p>
         </div>
       </div>
+      {open && (
+        <ul className={classes.field}>
+          {items.map((item) => (
+            <li className={classes.span} key={item.id}>
+              <span className={classes.item}>Service: {item.title}</span>
+              <span className={classes.item}>Service length: {item.time}</span>
+              <span className={classes.item}>Price: ${item.price}</span>
+              <span className={classes.item}>Description: {item.description}</span>
+              <button
+                className={classes.smallBtn}
+                type="button"
+                onClick={() => handleOnClick(item)}
+              >
+                edit
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 export default Services;
--- client/src/Pages/servicePage.js
+++ client/src/Pages/servicePage.js
@@ -2,25 +2,40 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Services from '../Components/service';
 
-const serviceInfo = {
-  labels: 'Threading',
-  microServices: 'eyebrows',
-  time: 15,
-  price: 10,
-};
+const serviceInfo = [
+  {
+    labels: 'Threading',
+    microServices: [
+      {
+        id: 1,
+        title: 'Eyebrows',
+        time: '15',
+        price: '10',
+        description: 'you get yours eyebrows threading dun dun DUUUUN',
+      },
+      {
+        id: 2,
+        title: 'Chin',
+        time: '15',
+        price: '7',
+        description: 'you get yours chin threading dun dun DUUUUN',
+      },
+    ],
+  },
+];
 
 const useStyles = makeStyles({
   root: {
     marginTop: 40,
   },
 });
-
+console.log(serviceInfo.labels);
 const ServicePage = () => {
   const classes = useStyles();
   return (
     <div>
       <div data-testid="services" className={classes.root}>
-        <Services  />
+        <Services label={serviceInfo[0].labels} items={serviceInfo[0].microServices} />
       </div>
     </div>
   );
