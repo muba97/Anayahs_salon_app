8f1046331c6d4863ce4cf2c8b127c2d4069298de
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2020-07-23 11:48:54 -0500

    [TESTS] :dart: Added error state test for ProfilePage

--- client/src/tests/Pages/ProfilePage.test.js
+++ client/src/tests/Pages/ProfilePage.test.js
@@ -4,7 +4,6 @@ import { MockedProvider } from '@apollo/client/testing';
 import { USER_INFO } from '../../GQLqueries/user';
 import ProfilePage from '../../Pages/ProfilePage';
 
-const userId = '5f0b8d54dd147401c51afd9f';
 const id = '1';
 
 const USER_INFO_RESPONSE = {
@@ -32,7 +31,7 @@ describe('<ProfilePage /> Tests', () => {
     cleanup();
   });
 
-  test('should render final state', async () => {
+  test('should render loading & final state', async () => {
     const { findByTestId } = render(
       <MockedProvider mocks={[mocks]} addTypename={false}>
         <ProfilePage id="1" />
@@ -44,5 +43,24 @@ describe('<ProfilePage /> Tests', () => {
 
     const profilePage = await findByTestId('profilePage');
     expect(profilePage).toBeInTheDocument();
+  });
+
+  test('should render error state', async () => {
+    const errorMock = {
+      request: {
+        query: USER_INFO,
+        variables: { id },
+      },
+      error: new Error('Test error'),
+    };
+
+    const { findByText } = render(
+      <MockedProvider mocks={[errorMock]} addTypename={false}>
+        <ProfilePage id="1" />
+      </MockedProvider>
+    );
+
+    const fetchError = await findByText('Error: Test error');
+    expect(fetchError).toBeInTheDocument();
   });
 });
